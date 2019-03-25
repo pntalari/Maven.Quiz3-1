83262c53646e830f24fa401e3105cdf13bf20d6e
Nirmala <nirmala.talari@gmail.com>
2019-03-25 09:49:33 -0400

    vowelutilsDone

--- src/main/java/rocks/zipcode/io/quiz3/fundamentals/VowelUtils.java
+++ src/main/java/rocks/zipcode/io/quiz3/fundamentals/VowelUtils.java
@@ -4,20 +4,42 @@ package rocks.zipcode.io.quiz3.fundamentals;
  * @author leon on 09/12/2018.
  */
 public class VowelUtils {
+    private static final Character[] vowelsArr = {'a', 'e', 'i', 'o', 'u',
+            'A', 'E', 'I', 'O', 'U'};
+
     public static Boolean hasVowels(String word) {
-        return null;
+        for (int i = 0; i <word.length() ; i++) {
+            if(isVowel(word.charAt(i))){
+                return true;
+            }
+        }
+        return false;
     }
 
     public static Integer getIndexOfFirstVowel(String word) {
-        return null;
+        int index = -1;
+        for (int i = 0; i < word.length(); i++) {
+            if (isVowel(word.charAt(i))) {
+                index = i;
+                break;
+            }
+        }
+        return index;
     }
 
 
     public static Boolean startsWithVowel(String word) {
-        return null;
+        if (isVowel(word.charAt(0)))
+            return true;
+        return false;
     }
 
     public static Boolean isVowel(Character character) {
-        return null;
+        for (Character c : vowelsArr) {
+            if (character.equals(c)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
