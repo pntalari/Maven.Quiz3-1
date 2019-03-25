3c3cbd6b24e16d9a0dfadd64759e1c3abfec4956
Nirmala <nirmala.talari@gmail.com>
2019-03-25 13:04:04 -0400

    StudentWIP

--- src/main/java/rocks/zipcode/io/quiz3/arrays/WaveGenerator.java
+++ src/main/java/rocks/zipcode/io/quiz3/arrays/WaveGenerator.java
@@ -1,10 +1,25 @@
 package rocks.zipcode.io.quiz3.arrays;
 
+import rocks.zipcode.io.quiz3.fundamentals.StringUtils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author leon on 09/12/2018.
  */
 public class WaveGenerator {
     public static String[] wave(String str) {
-        return null;
+        String strLower = str.toLowerCase();
+        List<String> listWave = new ArrayList<>();
+        //   String[] arrWave = new String[]
+
+        for (int i = 0; i < strLower.length(); i++) {
+            if (Character.isLetter(strLower.charAt(i))) {
+                listWave.add(StringUtils.capitalizeNthCharacter(strLower, i));
+            }
+        }
+        return listWave.toArray(new String[listWave.size()]);
     }
 }
--- src/main/java/rocks/zipcode/io/quiz3/collections/Student.java
+++ src/main/java/rocks/zipcode/io/quiz3/collections/Student.java
@@ -1,27 +1,46 @@
 package rocks.zipcode.io.quiz3.collections;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author leon on 10/12/2018.
  */
 public class Student {
+   List<Lab> labList;
 
     public Student() {
         this(null);
     }
 
     public Student(List<Lab> labs) {
+      this.labList = new ArrayList<>();
     }
 
     public Lab getLab(String labName) {
+        Lab lab = get
+        if(this.labList.contains()){
+            Integer index = labList.indexOf(labName);
+            return labList.get(index);
+        }
+
         return null;
     }
 
     public void setLabStatus(String labName, LabStatus labStatus) {
+        Lab lab = getLab(labName);
+        if(lab == null){
+            throw new UnsupportedOperationException();
+        }
+        lab.setStatus(labStatus);
     }
 
     public void forkLab(Lab lab) {
+        lab.setStatus(LabStatus.PENDING);
+        this.labList.add(lab);
+
     }
 
     public LabStatus getLabStatus(String labName) {
